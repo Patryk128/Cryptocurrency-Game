172ef77de1b0c0bc8d631cb23c9bb7551442b88c
Patryk Krawczyk <patrykkrawczykk157@gmail.com>
2025-05-04 18:20:11 +0200

    add env

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2F0-T2kKFwUqS71jJrjN6c-HHQT91eKI",
-  authDomain: "crypto-game-5cb71.firebaseapp.com",
-  projectId: "crypto-game-5cb71",
-  storageBucket: "crypto-game-5cb71.firebasestorage.app",
-  messagingSenderId: "70409075724",
-  appId: "1:70409075724:web:1341c15fd692b0f8bc8229",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
